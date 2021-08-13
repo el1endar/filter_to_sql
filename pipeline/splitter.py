15def8c6a364982e84cc79799c698174b0499e10
el1endar <59345433+el1endar@users.noreply.github.com>
2021-08-13 21:18:34 +0200

    Update splitter.py

--- pipeline/splitter.py
+++ pipeline/splitter.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
-# import time
+import time
+import datetime
 
 
 class Expression(object):
@@ -10,7 +11,8 @@ class Expression(object):
         self.operators_dict = {"and": "and", "or": "or", "eq": "==",   # we can save it in config and get this info from config
                                "gt": ">", "ge": ">=", "lt": "<",
                                "le": "<=", "between": "between",
-                               "is_null": "is_null", "not": "not"}
+                               "is_null": "is_null", "not": "not",
+                               "in": "in"}
         self.multiple_condition_operators = ["and", "or", "not"]
         self.negative_operator = []
         self._count = 0
@@ -89,7 +91,17 @@ class Expression(object):
         if operator == "between":
 
             filter = {"field": split_filter[0].strip(), "op": operator,
-                      'values': [split_filter[1].strip(), split_filter[2].strip()]}
+                      'values': [self.check_value_type(value=split_filter[1].strip()),
+                                 self.check_value_type(value=split_filter[2].strip())]}
+
+        elif operator == "in":
+
+            value_list = []
+            for value in split_filter[1:]:
+                value_list.append(self.check_value_type(value=value.strip()))
+
+            filter = {"field": split_filter[0].strip(), "op": operator,
+                      'values': value_list}
 
         elif operator in ["is_null"]:
 
@@ -98,8 +110,9 @@ class Expression(object):
         else:
 
             filter = {"field": split_filter[0].strip(), "op": operator,
-                      'value': split_filter[1].strip()}
-
+                      'value': self.check_value_type(value=split_filter[1].strip())}
+            print(filter.get("value"))
+            print(type(self.check_value_type(value=split_filter[1].strip())))
         return filter
 
     def parse_one_two_expr(self, filter: str):
@@ -134,35 +147,76 @@ class Expression(object):
 
         return work_dir
 
+    @staticmethod
+    def check_value_type(value):
+
+        try:
+            return int(value)
+
+        except ValueError:
+            pass
+
+        try:
+            return float(value)
+
+        except ValueError:
+            pass
+
+        try:
+
+            return datetime.datetime.strptime(value[1:-1], '%Y-%m-%d').date()
+
+        except ValueError:
+            pass
+
+        try:
+
+            return datetime.datetime.strptime(value[1:-1], '%Y-%m-%dT%H:%M:%S')
+
+        except ValueError:
+            pass
+
+        try:
+            return bool(value)
+
+        except ValueError:
+            pass
+
+        return value
+
 
 print("test_0")
 test_0 = Expression(filter="124").resolve()
 print(test_0)
 
 print("test_1")
-test_1 = Expression(filter="eq(startDate,2021-01-03)").resolve()
+test_1 = Expression(filter="eq(startDate,'2021-01-03')").resolve()
 print(test_1)
 
 print("test_2")
-test_2 = Expression(filter="between(startDate,2021-01-03,2021-04-20)").resolve()
+test_2 = Expression(filter="between(startDate,'2021-01-03','2021-04-20')").resolve()
 print(test_2)
 
 print("test_3")
-test_3 = Expression(filter="not(eq(startDate,2021-01-03))").resolve()
+test_3 = Expression(filter="not(eq(startDate,'2021-01-03'))").resolve()
 print(test_3)
 
 print("test_4")
-test_4 = Expression(filter="not(between(startDate,2021-01-03,2021-04-20))").resolve()
+test_4 = Expression(filter="not(between(startDate,'2021-01-03','2021-04-20'))").resolve()
 print(test_4)
 
 print("test_5")
-test_5 = Expression(filter="and(gt(ownerName,Mike(Senczyk)),between(startDate,2021-01-03,2021-04-20))").resolve()
+test_5 = Expression(filter="and(gt(ownerName,Mike(Senczyk)),between(startDate,'2021-01-03','2021-04-20'))").resolve()
 print(test_5)
 
 print("test_6")
-test_6 = Expression(filter="and(gt(ownerName,Mike(Senczyk)),or(between(startDate,2021-01-03,2021-04-20),not(gt(ownerName,Mike(Senczyk)))))").resolve()
+test_6 = Expression(filter="and(gt(ownerName,Mike(Senczyk)),or(between(startDate,'2021-01-03','2021-04-20'),not(gt(ownerName,Mike(Senczyk)))))").resolve()
 print(test_6)
 
 print("test7")
-test_7 = Expression(filter="and(eq(startDate,2021-01-03),gt(ownerName,55),not(between(startDate,2021-01-03,2021-04-20)))").resolve()
+test_7 = Expression(filter="and(eq(startDate,'2021-05-23T11:23:23'),gt(ownerName,55),not(between(startDate,'2021-01-03','2021-04-20')))").resolve()
 print(test_7)
+
+print("test_8")
+test_8 = Expression(filter="and(eq(startDate,'2021-05-23T11:23:23'),gt(ownerName,55),not(in(startDate,'2021-01-03','2021-04-20','2021-04-22')))").resolve()
+print(test_8)
